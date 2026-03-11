175b11cdd36856a3e0f067c12c96f2813b693bb9
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-03-11 20:33:31 -0300

    Add post service

--- src/app/services/post-service.ts
+++ src/app/services/post-service.ts
@@ -1,6 +1,33 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { PostRequest } from '../models/post-request';
+import { Observable } from 'rxjs';
+import { PostDet } from '../models/post-det';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PostService {}
+export class PostService {
+  private readonly apiUrl = 'http://localhost:8080/api/posts';
+  private http = inject(HttpClient);
+
+  addPost(post: PostRequest): Observable<PostDet> {
+    return this.http.post<PostDet>(this.apiUrl, post);
+  }
+
+  getPostById(id: number): Observable<PostDet> {
+    return this.http.get<PostDet>(`${this.apiUrl}/${id}`);
+  }
+
+  listPosts(): Observable<PostDet[]> {
+    return this.http.get<PostDet[]>(this.apiUrl);
+  }
+
+  updatePost(id: number, post: Partial<PostRequest>): Observable<PostDet> {
+    return this.http.patch<PostDet>(`${this.apiUrl}/${id}`, post);
+  }
+
+  deletePost(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
